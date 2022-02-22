616353758e98c450984b2d9bdcf051ca98c5c7f8
Oxypetalum7 <yuu.dtm.7@gmail.com>
2022-02-23 04:18:23 +0900

    fix smartphone Toppage

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import { Paper, Stack, Typography } from "@mui/material"
+import { Paper, Stack, Typography, Button } from "@mui/material"
 import { Box , Grid} from "@mui/material"
 import { useEffect} from "react"
 import { Link } from "react-router-dom"
@@ -24,22 +24,22 @@ const Home = (props) => {
                     <Grid item className="skewed-background" >
                         <Box item xs={12} sx={{position:"relative", mt:"20vh", ml:"25vw"}}>
                             <Link to="/biography">
-                                <Typography componrnt="title-index" variant="h2" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
+                                <Typography variant="h2" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
                                     Biography
                                 </Typography>
                             </Link>
                             <Link to="/activity">
-                                <Typography componrnt="title-index" variant="h2" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
+                                <Typography variant="h2" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
                                     Activity
                                 </Typography>
                             </Link>
                             <Link to="/works">
-                                <Typography componrnt="title-index" variant="h2" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
+                                <Typography variant="h2" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
                                     Works
                                 </Typography>
                             </Link>
                             <Link to="/link">
-                                <Typography componrnt="title-index" variant="h2" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
+                                <Typography variant="h2" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
                                     Link
                                 </Typography>
                             </Link>
@@ -47,42 +47,57 @@ const Home = (props) => {
                     </Grid>
                 </Grid>
             </Grid>
-            <Paper sx={{display:{xs:"block", md:"none"}}}>
-                <Grid container  justifyContent="center">
-                    <Grid item xs={12} sx={{display:'inline', mt:10, textAlign:"center"}}>
-                        <Typography className="title" noWrap variant="h4" sx={{color:"#FFFFFF", fontWeight:"bold"}}>
-                            Oxypetalum7
-                        </Typography>
-                        <Typography className="title" noWrap variant="h2" sx={{color:"#FFFFFF", fontWeight:"bold"}}>
-                            Portfolio
-                        </Typography>
+            <Grid container sx={{display:{xs:"inline", md:"none"}}}>
+                <Paper height="100%">
+                    <Grid item xs={12} height="100vh">
+                        <Grid container height="100%" direction="column" justifyContent="center">
+                            <Grid item xs={3}> 
+                                <Box sx={{ textAlign:"center"}}>
+                                    <Typography className="title" noWrap variant="h4" sx={{color:"#FFFFFF", fontWeight:"bold"}}>
+                                        Oxypetalum7
+                                    </Typography>
+                                    <Typography className="title" noWrap variant="h2" sx={{color:"#FFFFFF", fontWeight:"bold"}}>
+                                        Portfolio
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Stack direction='column' spacing={4} justifyContent='center' textAlign='center' >
+                                    <Link to="/biography">
+                                        <Button>
+                                            <Typography variant="h3" sx={{ color:"#FFFFFF", fontWeight:"bold"}}>
+                                                Biography
+                                            </Typography>
+                                        </Button>
+                                    </Link>
+                                    <Link to="/activity">
+                                        <Button>
+                                            <Typography variant="h3" sx={{ color:"#FFFFFF", fontWeight:"bold"}}>
+                                                Activity
+                                            </Typography>
+                                        </Button>
+                                    </Link>
+                                    <Link to="/works">
+                                        <Button>
+                                        <Typography variant="h3" sx={{ color:"#FFFFFF", fontWeight:"bold"}}>
+                                            Works
+                                        </Typography>
+                                        </Button>
+                                    </Link>
+                                    <Link to="/link">
+                                        <Button>
+                                        <Typography variant="h3" sx={{ color:"#FFFFFF", fontWeight:"bold"}}>
+                                            Link
+                                        </Typography>
+                                        </Button>
+                                    </Link>
+                                </Stack>
+                            </Grid>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sx={{ mt:10 }}>
-                        <Stack direction='column' justifyContent='center' textAlign='center'>
-                            <Link to="/biography">
-                                <Typography variant="h3" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
-                                    Biography
-                                </Typography>
-                            </Link>
-                            <Link to="/activity">
-                                <Typography variant="h3" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
-                                    Activity
-                                </Typography>
-                            </Link>
-                            <Link to="/works">
-                                <Typography variant="h3" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
-                                    Works
-                                </Typography>
-                            </Link>
-                            <Link to="/link">
-                                <Typography variant="h3" sx={{my:4, color:"#FFFFFF", fontWeight:"bold"}}>
-                                    Link
-                                </Typography>
-                            </Link>
-                        </Stack>
-                    </Grid>
-                </Grid>
-            </Paper>
+                </Paper>
+            </Grid>
+            
         </div>
     )
 }
